b24713a47615a08c8f23340016ce14ec99ea43c9
osparking <jbpark03@gmail.com>
2023-08-19 18:01:13 +0900

    initial - 3 dependencies

---
+++ src/test/java/space/bum/c6_ex1/Ss2022C7Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bum.c6_ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss2022C7Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
