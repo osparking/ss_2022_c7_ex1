29b35eba3e41afb818bb410c8fe0c0579fc04766
osparking <jbpark03@gmail.com>
2023-08-19 18:28:40 +0900

    require all request authenticated

---
+++ src/main/java/space/bum/c7_ex1/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package space.bum.c7_ex1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http.httpBasic().and()
+				.authorizeRequests().anyRequest().authenticated().and()
+				.build();
+	}
+
+}
