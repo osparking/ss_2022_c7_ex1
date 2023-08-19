e28c8a6570eebe654076906c8526ee5b28f67e39
osparking <jbpark03@gmail.com>
2023-08-19 18:35:46 +0900

    create park's userdetails

--- src/main/java/space/bum/c7_ex1/config/SecurityConfig.java
+++ src/main/java/space/bum/c7_ex1/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package space.bum.c7_ex1.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +15,15 @@ public class SecurityConfig {
 		return http.httpBasic().and()
 				.authorizeRequests().anyRequest().authenticated().and()
 				.build();
+	}
+	
+	@Bean
+	UserDetailsService userDetailsService() {
+		UserDetails park = User.withUsername("park")
+				.password("1234")
+				.authorities("read")
+				.build();
+		return null;
 	}
 
 }
